077dfafdfc16b1cef79f9cde8c767df29d293246
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-07-15 23:49:43 -0300

    Primero

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: 'counter.component.html',
+})
+export class CounterComponent {
+  constructor() {}
+  public counter: number = 10;
+  increaseBy(value: number): void {
+    this.counter += value;
+  }
+  resetCounter(): void {
+    this.counter = 10;
+  }
+}
